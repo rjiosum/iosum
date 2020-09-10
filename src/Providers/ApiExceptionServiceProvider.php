b450b79ca6d979fea96e8d2b0bc67e7bcfc80371
Raj <rj.iosum@gmail.com>
2020-09-10 23:26:49 +0100

    Load lang and view file in service provider

--- src/Providers/ApiExceptionServiceProvider.php
+++ src/Providers/ApiExceptionServiceProvider.php
@@ -4,11 +4,32 @@
 namespace Iosum\ApiException\Providers;
 
 
+use Illuminate\Http\Request;
 use Iosum\Base\Providers\BaseServiceProvider;
 
 
 class ApiExceptionServiceProvider extends BaseServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     * @param Request $request
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function boot(Request $request): void
+    {
+        $this->loadTranslationsFrom($this->dirPath(__DIR__) . 'resources/lang', 'api-exception');
 
+        $this->loadViewsFrom($this->dirPath(__DIR__).'resources/views', 'api-exception');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    public function register(): void
+    {
+
+    }
 
 }
