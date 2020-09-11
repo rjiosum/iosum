b418d29b8d6319a50cc31c02f61aac4a886a70ad
Raj <rj.iosum@gmail.com>
2020-09-11 12:04:52 +0100

    Removed BaseServiceProvider dependency

--- src/Providers/ApiExceptionServiceProvider.php
+++ src/Providers/ApiExceptionServiceProvider.php
@@ -1,31 +1,29 @@
 <?php declare(strict_types=1);
 
-
 namespace Iosum\ApiException\Providers;
 
+use Illuminate\Support\ServiceProvider;
+use Iosum\Base\Traits\BaseProvider;
 
-use Illuminate\Http\Request;
-use Iosum\Base\Providers\BaseServiceProvider;
 
-
-class ApiExceptionServiceProvider extends BaseServiceProvider
+class ApiExceptionServiceProvider extends ServiceProvider
 {
+    use BaseProvider;
+
     /**
      * Bootstrap any application services.
-     * @param Request $request
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     *
      */
-    public function boot(Request $request): void
+    public function boot(): void
     {
         $this->loadTranslationsFrom($this->dirPath(__DIR__) . 'resources/lang', 'api-exception');
 
-        $this->loadViewsFrom($this->dirPath(__DIR__).'resources/views', 'api-exception');
+        $this->loadViewsFrom($this->dirPath(__DIR__) . 'resources/views', 'api-exception');
     }
 
     /**
      * Register any application services.
      *
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     public function register(): void
     {
